07d6a0c72f310e8cf5bc3afd9ec4ae45483ff857
ebotzocm <ebotzocm@miumg.edu.gt>
2025-09-18 23:37:53 -0600

    Cambios actualizados

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -10,6 +10,8 @@ interface EnvVars {
   STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
 
+  NATS_SERVERS: string[];
+
 }
 
 const envsSchema = joi.object({
@@ -18,10 +20,15 @@ const envsSchema = joi.object({
   STRIPE_SUCCESS_URL: joi.string().required(),
   STRIPE_CANCEL_URL: joi.string().required(),
   STRIPE_ENDPOINT_SECRET: joi.string().required(),
+
+  NATS_SERVERS: joi.array().items( joi.string() ).required(),
 })
 .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({ 
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
 
 if ( error ) {
@@ -37,4 +44,6 @@ export const envs = {
   stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
   stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+
+  natsServers: envVars.NATS_SERVERS,
 }
